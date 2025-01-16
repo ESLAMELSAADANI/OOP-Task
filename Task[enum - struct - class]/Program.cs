9529b1da1019d9f8ea5eabc47f488bd92bf8f641
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2025-01-16 20:51:26 +0200

    Update Console Screen

--- Task[enum - struct - class]/Program.cs
+++ Task[enum - struct - class]/Program.cs
@@ -8,15 +8,19 @@
             DateTime borrowDate = new DateTime(2025, 1, 1);
             BorrowedBook borrowedBook = new BorrowedBook(1, book, "Ahmed Ashraf", borrowDate);
 
-            Console.WriteLine($"The {borrowedBook.BookDetails.Title} Book Borrowed!");
+            Console.WriteLine($"Book Title: {borrowedBook.BookDetails.Title}");
+            Console.WriteLine($"Author: {borrowedBook.BookDetails.Author}");
+            Console.WriteLine($"ISBN: {borrowedBook.BookDetails.ISBN}");
+
+            Console.WriteLine($"\nThe {borrowedBook.BookDetails.Title} Book Borrowed at {borrowDate.Day}/{borrowDate.Month}/{borrowDate.Year}!");
             borrowedBook.CheckOut();
 
             DateTime returnDate = new DateTime(2025, 1, 16);
             int borrowDuration = borrowedBook.CalculateBorrowDuration(returnDate);
 
-            Console.WriteLine($"The {borrowedBook.BookDetails.Title} Book Returned!");
+            Console.WriteLine($"\nThe {borrowedBook.BookDetails.Title} Book Returned at {returnDate.Day}/{returnDate.Month}/{returnDate.Year}");
             borrowedBook.ReturnItem();
-            Console.WriteLine($"The Borrowing Duration is : {borrowDuration} Day");
+            Console.WriteLine($"\nThe Borrowing Duration is : {borrowDuration} Day");
         }
     }
 }
